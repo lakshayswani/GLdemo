3d72b8d23425ebe0ecb6da5f8e55a799593f306a
Lakshay Swani <lakshay.swani@synapse.com>
2016-10-26 10:42:10 +0530

    Adding project

---
+++ src/main/java/org/gradle/controller/SampleController.java
@@ -0,0 +1,43 @@
+package org.gradle.controller;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.gradle.pojo.User;
+
+@Controller
+public class SampleController {
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    private ArrayList<User> users =new ArrayList<>();
+    
+    @GetMapping("/adduser")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "adduser";
+    }
+
+    @PostMapping(value="/adduser", params="action=Add")
+    public String addMore(Model model, @ModelAttribute User user) {
+    	user.setId((int)counter.incrementAndGet());
+        users.add(user);
+    	model.addAttribute("userList", users);
+        model.addAttribute("user", new User());
+        return "adduser";
+    }
+    @PostMapping(value="/adduser", params="action=View")
+    @ResponseBody
+    public ArrayList<User> json() {
+    	return users;
+    }
+    
+
+}
---
+++ src/main/java/org/gradle/pojo/User.java
@@ -0,0 +1,32 @@
+package org.gradle.pojo;
+
+public class User {
+
+	int id;
+	String firstName, lastName, phoneNo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+}
